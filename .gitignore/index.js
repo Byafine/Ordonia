64f061d5b87133d4cce433c35120ac987e991495
Byafine <42322434+Byafine@users.noreply.github.com>
2018-08-12 19:27:53 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,12 @@
+const discord = require('discord.js');
+
+const client = new discord.Client();
+
+var prefix = "/";
+
+client.login("NDc1NzI5NTE3MDA5MzA1NjAw.DkjTtw.i7zEjmuCEDNCg1Zx-cmGGnJcrEk");
+
+
+client.on("ready" , () => {
+    console.log("Je suis prêt !");
+    client.user.setGame("/aide");
